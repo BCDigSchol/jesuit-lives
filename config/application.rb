e8e1ec483be31ef0d3e02b33dbc9cf62677f3b17
jessemartinez <mjesse@gmail.com>
2021-04-06 11:08:39 -0400

    attempt to avoid precompile bug on sandbox server

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module JesuitLives
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.configure do |env|
+      env.export_concurrent = false
+    end
   end
 end
