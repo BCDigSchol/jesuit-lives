dfd79604932aa7d0cb09467687f4ef9fbcd1fd93
jessemartinez <mjesse@gmail.com>
2021-04-06 11:35:00 -0400

    remove env condition for seeding users, roles, staticpages

--- db/seeds/020_users.rb
+++ db/seeds/020_users.rb
@@ -1,43 +1,44 @@
-case Rails.env
-when "development"
-  admin_role = Role.find_by_name('admin')
-  supervisor_role = Role.find_by_name('supervisor')
-  editor_role = Role.find_by_name('editor')
-  noaccess_role = Role.find_by_name('noaccess')
+#case Rails.env
+#when "development"
 
-  User.find_or_create_by(email: 'admin@test.com') do | user |
-    user.password = 'password'
-    user.password_confirmation = 'password'
-    user.name = 'Jimmy Admin'
-    user.role = admin_role
-  end
-  
-  User.find_or_create_by(email: 'supervisor1@test.com') do | user |
-    user.password = 'password'
-    user.password_confirmation = 'password'
-    user.name = 'Jane Supervisor'
-    user.role = supervisor_role
-  end
+admin_role = Role.find_by_name('admin')
+supervisor_role = Role.find_by_name('supervisor')
+editor_role = Role.find_by_name('editor')
+noaccess_role = Role.find_by_name('noaccess')
 
-  User.find_or_create_by(email: 'supervisor2@test.com') do | user |
-    user.password = 'password'
-    user.password_confirmation = 'password'
-    user.name = 'Julio Supervisor'
-    user.role = supervisor_role
-  end
-
-  User.find_or_create_by(email: 'editor@test.com') do | user |
-    user.password = 'password'
-    user.password_confirmation = 'password'
-    user.name = 'Nancy Editor'
-    user.role = editor_role
-  end
-
-  User.find_or_create_by(email: 'noaccess@test.com') do | user |
-    user.password = 'password'
-    user.password_confirmation = 'password'
-    user.name = 'Noah Noaccess'
-    user.role = noaccess_role
-  end
-              
+User.find_or_create_by(email: 'admin@test.com') do | user |
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.name = 'Jimmy Admin'
+  user.role = admin_role
 end
+
+User.find_or_create_by(email: 'supervisor1@test.com') do | user |
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.name = 'Jane Supervisor'
+  user.role = supervisor_role
+end
+
+User.find_or_create_by(email: 'supervisor2@test.com') do | user |
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.name = 'Julio Supervisor'
+  user.role = supervisor_role
+end
+
+User.find_or_create_by(email: 'editor@test.com') do | user |
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.name = 'Nancy Editor'
+  user.role = editor_role
+end
+
+User.find_or_create_by(email: 'noaccess@test.com') do | user |
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.name = 'Noah Noaccess'
+  user.role = noaccess_role
+end
+
+#end
