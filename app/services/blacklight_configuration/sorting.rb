3e008986fb2c5b7d71232ec638cd73505fd54195
Ben Florin <benjamin.florin@bc.edu>
2021-04-23 12:45:21 -0400

    Added date & name filtering. #2

--- app/services/blacklight_configuration/sorting.rb
+++ app/services/blacklight_configuration/sorting.rb
@@ -8,9 +8,8 @@ module BlacklightConfiguration
       # except in the relevancy case). Add the sort: option to configure a
       # custom Blacklight url parameter value separate from the Solr sort fields.
       config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
-      config.add_sort_field 'year-desc', sort: 'pub_date_si desc, title_si asc', label: 'year'
-      config.add_sort_field 'author', sort: 'author_si asc, title_si asc', label: 'author'
-      config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
+      config.add_sort_field 'name', sort: "#{SolrIndexer::Fields::LAST_NAME} asc, #{SolrIndexer::Fields::FIRST_NAME} asc", label: 'name'
+      config.add_sort_field 'birth', sort: "#{SolrIndexer::Fields::BIRTH_DATE_YEAR} asc", label: 'year of birth'
     end
   end
 end
