8bead5a643da828cf00f62dbae8536649abdb1f6
Ben Florin <benjamin.florin@bc.edu>
2021-05-09 15:46:02 -0400

    Spiffy up edit forms a little bit

--- app/grids/jesuits_grid.rb
+++ app/grids/jesuits_grid.rb
@@ -8,7 +8,10 @@ class JesuitsGrid
     # fill out more filters and columns once model is updated
     filter(:last_name, :string) { |value| where("last_name ilike ?", "%#{value}%") }
 
-    column(:label, :mandatory => true)
+    column(:jl_id, :mandatory => true)
+    column(:last_name, :mandatory => true, :order => true)
+    column(:first_name, :mandatory => true, :order => false)
+
 
     column(:actions, :html => true, :mandatory => true) do | jesuit |
         render :partial => "datagrid/actions", :locals => {:record => jesuit, :model => Jesuit}
