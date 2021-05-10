fed45daf1adaadc0aa8a08782631c9fa07feb998
Ben Florin <benjamin.florin@bc.edu>
2021-05-10 17:39:58 -0400

    Format birth-year refine page correctly. #31

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,13 @@
 module ApplicationHelper
-  def link_to_year_search(options={})
-    date_range_field = options[:field].to_s[/(.*)_display/, 1]
+  def link_to_year_search(options = {})
+    date_range_field = options[:field].to_s[/(.*)_date_display/, 1]
     year = options[:value].to_s[/(\d\d\d\d)/, 1]
-    begin_key = "range[#{date_range_field}_iti][begin]"
-    end_key = "range[#{date_range_field}_iti][end]"
+    year_field = "range[#{date_range_field}_year_iti]"
 
-    link_to options[:value][0], blacklight_path(begin_key => year, end_key => year)
+    link_to options[:value][0], blacklight_path(
+      "#{year_field}[begin]" => year,
+      "#{year_field}[end]" => year,
+      :search_field => 'all_fields'
+    )
   end
 end
