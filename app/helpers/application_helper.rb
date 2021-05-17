99dc0c5e96746a2f1307ab44e321cf065820d1d6
Ben Florin <benjamin.florin@bc.edu>
2021-05-17 14:22:03 -0400

    Open external links in new tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
       :search_field => 'all_fields'
     )
   end
+
+  def icon(icon, options = {})
+    file = File.read("node_modules/bootstrap-icons/icons/#{icon}.svg")
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] += " " + options[:class]
+    end
+    doc.to_html.html_safe
+  end
 end
