25e32725fe28f160e2d838c42fd3df6878fc8798
Ben Florin <benjamin.florin@bc.edu>
2021-05-24 10:58:46 -0400

    Add reverse date sort. Fixes #41

--- app/services/blacklight_configuration/sorting.rb
+++ app/services/blacklight_configuration/sorting.rb
@@ -7,9 +7,10 @@ module BlacklightConfiguration
       # whether the sort is ascending or descending (it must be asc or desc
       # except in the relevancy case). Add the sort: option to configure a
       # custom Blacklight url parameter value separate from the Solr sort fields.
-      config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
-      config.add_sort_field 'name', sort: "#{SolrIndexer::Fields::LAST_NAME} asc, #{SolrIndexer::Fields::FIRST_NAME} asc", label: 'name'
-      config.add_sort_field 'birth', sort: "#{SolrIndexer::Fields::BIRTH_DATE_YEAR} asc", label: 'year of birth'
+      config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'Relevance'
+      config.add_sort_field 'name', sort: "#{SolrIndexer::Fields::LAST_NAME} asc, #{SolrIndexer::Fields::FIRST_NAME} asc", label: 'Name'
+      config.add_sort_field 'birth_asc', sort: "#{SolrIndexer::Fields::BIRTH_DATE_YEAR} asc", label: 'Year of birth (ascending)'
+      config.add_sort_field 'birth_desc', sort: "#{SolrIndexer::Fields::BIRTH_DATE_YEAR} desc", label: 'Year of birth (descending)'
     end
   end
 end
