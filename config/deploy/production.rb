f5515c8ab60ad7b01a8121329e925ad0b57fdf02
Benjamin Florin <benjamin.florin@gmail.com>
2022-08-12 10:40:08 -0400

    Configure production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,10 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "jesuit-lives.lontracanadensis.net", user: "rails", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+#
+set :deploy_to,  "/home/rails/#{fetch :application}"
+set :rails_env, "sandbox"
 
 
 # role-based syntax
